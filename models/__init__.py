da4bbd2cd89e4804d82077c205402b9bdb8971ce
soufiane amabrra <manga.sfn@gmail.com>
2025-09-27 18:10:26 +0100

    author route

---
+++ models/__init__.py
@@ -0,0 +1,10 @@
+from .error_handlers import (
+    handle_database_error,
+    handle_validation_error,
+    handle_not_found_error,
+    handle_conflict_error
+)
+
+
+
+
---
+++ routes/__init__.py
@@ -0,0 +1,5 @@
+from .books_api import books_api
+from .categories_api import categories_api  
+from .authors_api import authors_api
+
+
