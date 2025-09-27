7a7d0d53590b4e54e81ac5ef7211a1cc5aac35d9
soufiane amabrra <manga.sfn@gmail.com>
2025-09-27 02:58:47 +0100

    fix stractor

---
+++ database/seed/tabels_Init.sql
@@ -0,0 +1,107 @@
+-- language table
+CREATE TABLE IF NOT EXISTS languages (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(45) NOT NULL
+);
+
+
+-- user table
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(80) UNIQUE NOT NULL,
+    email VARCHAR(120) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL, 
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE collections (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    description TEXT,
+    user_id INTEGER NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT fk_user
+        FOREIGN KEY(user_id)
+        REFERENCES users(id)
+        ON DELETE CASCADE, 
+
+    UNIQUE (user_id, name)
+);
+
+-- Table for unique authors
+CREATE TABLE authors (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL UNIQUE
+);
+
+-- Table for unique categories/genres
+CREATE TABLE categories (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL UNIQUE
+);
+
+-- books table
+CREATE TABLE books (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    publication_year INTEGER,
+    cover_id BIGINT, 
+    open_library_id VARCHAR(50) NOT NULL UNIQUE,
+    language_id INTEGER,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_language
+        FOREIGN KEY(language_id)
+        REFERENCES languages(id)
+        ON DELETE SET NULL 
+);
+
+-- Junction table for the many-to-many relationship between books and authors
+CREATE TABLE book_authors (
+    book_id INTEGER NOT NULL,
+    author_id INTEGER NOT NULL,
+    PRIMARY KEY (book_id, author_id), -- Ensures each author is linked to a book only once
+    CONSTRAINT fk_book
+        FOREIGN KEY(book_id)
+        REFERENCES books(id)
+        ON DELETE CASCADE, -- If a book is deleted, remove its author links
+    CONSTRAINT fk_author
+        FOREIGN KEY(author_id)
+        REFERENCES authors(id)
+        ON DELETE CASCADE -- If an author is deleted, remove their book links
+);
+
+-- Junction table for the many-to-many relationship between books and categories
+CREATE TABLE book_categories (
+    book_id INTEGER NOT NULL,
+    category_id INTEGER NOT NULL,
+    PRIMARY KEY (book_id, category_id), -- Ensures each category is linked to a book only once
+    CONSTRAINT fk_book
+        FOREIGN KEY(book_id)
+        REFERENCES books(id)
+        ON DELETE CASCADE,
+    CONSTRAINT fk_category
+        FOREIGN KEY(category_id)
+        REFERENCES categories(id)
+        ON DELETE CASCADE
+);
+
+
+-- Junction table for the many-to-many relationship between collections and books
+CREATE TABLE collection_books (
+    collection_id INTEGER NOT NULL,
+    book_id INTEGER NOT NULL,
+    added_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+
+    PRIMARY KEY (collection_id, book_id),
+
+    CONSTRAINT fk_collection
+        FOREIGN KEY(collection_id)
+        REFERENCES collections(id)
+        ON DELETE CASCADE,
+
+    CONSTRAINT fk_book
+        FOREIGN KEY(book_id)
+        REFERENCES books(id)
+        ON DELETE CASCADE
+);
